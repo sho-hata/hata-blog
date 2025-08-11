267682b93bcf447cb84b944e73208b5a4d492815
sho-hata <syouki100241@gmail.com>
2025-08-11 22:43:46 +0900

    add bluesky

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,35 +1,42 @@
-import { Article } from "../components/article";
-import { GithubIcon, NoteIcon, SizuMeIcon, XIcon } from "../components/icons";
-import { getAllArticles } from "../lib/article";
+import { Article } from '../components/article';
+import {
+  BlueSkyIcon,
+  GithubIcon,
+  NoteIcon,
+  SizuMeIcon,
+  XIcon,
+} from '../components/icons';
+import { getAllArticles } from '../lib/article';
 
 export default function Top() {
   const imageUrl = import.meta.env.PROD
-    ? "/static/me.png"
-    : "/app/static/img/me.png";
+    ? '/static/me.png'
+    : '/app/static/img/me.png';
 
   const articles = getAllArticles();
   return (
-    <div class={"px-6 mt-6"}>
+    <div class={'px-6 mt-6'}>
       <section class="py-12 bg-white rounded-lg shadow-md">
-        <div class={"mx-8"}>
-          <div class={"flex items-center space-x-4 mb-8"}>
+        <div class={'mx-8'}>
+          <div class={'flex items-center space-x-4 mb-8'}>
             <img
               src={imageUrl}
-              alt={"Profile"}
-              class={"w-20 h-20 rounded-full"}
+              alt={'Profile'}
+              class={'w-20 h-20 rounded-full'}
             />
-            <div class={"flex flex-col gap-2"}>
-              <span class={"text-sm md:text-lg font-semibold"}>Shoki Hata</span>
-              <p class={"text-sm md:text-base"}>
+            <div class={'flex flex-col gap-2'}>
+              <span class={'text-sm md:text-lg font-semibold'}>Shoki Hata</span>
+              <p class={'text-sm md:text-base'}>
                 GoやTypeScriptでソフトウェアを書いているHataです。金融領域にいます。
                 このブログでは、雑多な技術ネタや、とりとめもないことを記録します。
               </p>
-              <a href="/about" class={"text-sm md:text-base underline"}>
+              <a href="/about" class={'text-sm md:text-base underline'}>
                 私について
               </a>
             </div>
           </div>
-          <div class={"flex gap-2 items-center pb-8 border-b"}>
+          <div class={'flex gap-2 items-center pb-8 border-b'}>
+            <BlueSkyIcon size={20} title="@sho_hata" />
             <a
               href="https://x.com/sho_hata_"
               target="_blank"
@@ -59,7 +66,7 @@ export default function Top() {
               <SizuMeIcon size={20} title="@sho_hata" />
             </a>
           </div>
-          <div class={"mt-6 flex flex-col gap-8"}>
+          <div class={'mt-6 flex flex-col gap-8'}>
             {articles.map(({ entryName, frontmatter }) => {
               if (entryName) {
                 return (
